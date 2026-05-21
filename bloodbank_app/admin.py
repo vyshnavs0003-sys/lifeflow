554fa121a69286d48fbd174554097e6e21e2e4c4
vyshnav s <vyshnavs0003@gmail.com>
2026-05-21 10:03:39 +0530

    feat: add bloodbank system models and did admin register

--- bloodbank_app/admin.py
+++ bloodbank_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Donor, Hospital, BloodInventrory, BloodRequest
 # Register your models here.
+
+admin.site.register(Donor)
+admin.site.register(Hospital)
+admin.site.register(BloodInventrory)
+admin.site.register(BloodRequest)
--- bloodbank_app/models.py
+++ bloodbank_app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Donor(models.Model):
+    donor_name = models.CharField(max_length=100)
+    blood_group = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10)
+    location = models.CharField(max_length=100)
+    age = models.IntegerField()
+    last_donation_date = models.DateField()
+
+    def __str__(self):
+        return f"{self.donor_name} - {self.blood_group}"
+    
+class Hospital(models.Model):
+    hospital_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    contact = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.hospital_name
+
+class BloodInventrory(models.Model):
+    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    blood_group = models.CharField(max_length=10)
+    units_available = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.hospital.hospital_name} - {self.blood_group}"
+
+class BloodRequest(models.Model):
+    requested_by = models.ForeignKey(User,on_delete=models.CASCADE)  
+    patient_name = models.CharField(max_length=100)
+    patient_id = models.CharField(max_length=50)
+    blood_group_needed= models.CharField(max_length=10)
+    units_needed= models.IntegerField()
+    hospital = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+    reason = models.TextField()
+    required_date = models.DateField()
+    contact_number = models.CharField(max_length=10)
+    status = models.CharField(max_length=20,default='Pending')
+
+    def __str__(self):
+        return self.patient_name
+
