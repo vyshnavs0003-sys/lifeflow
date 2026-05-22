196c17e45642e918bbe5dd27b52cbfd79aa067a0
vyshnav s <vyshnavs0003@gmail.com>
2026-05-22 12:11:12 +0530

    fix: corrected typo in RegisterForm fields

--- bloodbank_app/forms.py
+++ bloodbank_app/forms.py
@@ -29,7 +29,7 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField()
     class Meta:
         model = User
-        field = ['username','email','password1','password2']
+        fields = ['username','email','password1','password2']
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100) 
