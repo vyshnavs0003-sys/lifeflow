c344af45b09fa9934314b2379b9b481499092c68
vyshnav s <vyshnavs0003@gmail.com>
2026-05-26 06:46:48 +0530

    refactor: add more custom fields in RegisterForm

--- bloodbank_app/forms.py
+++ bloodbank_app/forms.py
@@ -26,10 +26,13 @@ class BloodInventoryForm(forms.ModelForm):
         fields = ['hospital','blood_group','units_available']   
 
 class RegisterForm(UserCreationForm):
+    first_name=forms.CharField(max_length=100)
+    last_name=forms.CharField(max_length=100)
     email = forms.EmailField()
+    phone = forms.CharField(max_length=10)
     class Meta:
         model = User
-        fields = ['username','email','password1','password2']
+        fields = ['first_name','last_name','username','email','phone','password1','password2']
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100) 
